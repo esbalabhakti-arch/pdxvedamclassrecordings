95ccfb8f56b4719ac01478a0175775e44a042bf1
esbalabhakti-arch <esbalabhakti@gmail.com>
2025-11-29 08:27:56 -0800

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,18 @@
-// data.js — list of Vedam class sessions
-
 window.vedamClasses = [
-  {
-    id: "2025-11-29",
-    displayDate: "Nov-29-2025",
-    image: "Images/20251129.jpg",
-    imageAlt: "Class Nov 22, 2025",
-  audio: 
-      	{ label: "1. 11292025 — Class Recording - Split phrases", file: "Audio/20251129_Split.ogg" },
-      	{ label: "2. 11292025 — Class Recording - Full Lines", file: "Audio/20251129_Full.ogg" }
-  }
-
-  // Add more sessions like this:
-  /*
-  {
-    id: "2025-11-29",
-    displayDate: "Nov-29-2025",
-    image: "Images/20251129.png",
-    imageAlt: "Mantra learned on Nov 29, 2025",
-    audio: "media/20251129.opus",
-    audioLabel: "2. 11292025 — Class Recording"
-  }
-  */
+    {
+        id: "2025-11-29",
+        displayDate: "Nov-29-2025",
+        image: "Images/20251129.jpg",
+        imageAlt: "Class Nov 22, 2025",
+        audio: [
+            { 
+                label: "1. 11292025 — Class Recording - Split phrases", 
+                file: "Audio/20251129_Split.ogg" 
+            },
+            { 
+                label: "2. 11292025 — Class Recording - Full Lines", 
+                file: "Audio/20251129_Full.ogg" 
+            }
+        ]
+    }
 ];
-
-
-
-
-
